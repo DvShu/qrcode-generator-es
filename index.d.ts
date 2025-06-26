667f874c8d98e1d245e60aa4769d02cd2afcc622
Tenny <tenny.shu@foxmail.com>
2025-06-26 17:38:59 +0800

    feat(QRCodeRender): 添加配置项设置方法并更新版本号
    添加新的 set 方法用于修改单个渲染配置项，同时更新 package.json 版本号至 1.1.2

--- index.d.ts
+++ index.d.ts
@@ -38,10 +38,14 @@ export declare function renderToImg(qrcode: qrcodegen.QrCode, option: RequiredOp
 /** 二维码渲染 */
 export declare class QRCodeRender {
     option: RequiredOption;
+    private _defaultOption;
     constructor(option: QRCodeRenderOption);
     /** 渲染二维码 */
     render(): HTMLElement;
+    /** 批量修改渲染配置 */
     setOption(option: Partial<QRCodeRenderOption>): void;
+    /** 修改渲染配置项 */
+    set<K extends keyof QRCodeRenderOption>(key: K, value: QRCodeRenderOption[K]): void;
     /** 添加数据并渲染二维码 */
     addData(data: string): HTMLElement;
     /** 重置二维码 */
